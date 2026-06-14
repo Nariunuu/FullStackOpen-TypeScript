efd5a7e946e9cd6715ad62ff469fd08efb4c66d4
khuevan2810 <khuevan2810@gmail.com>
2026-06-15 00:08:00 +0300

    5. WebBMI

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator.ts';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (!req.query.height || !req.query.weight || isNaN(height) || isNaN(weight)) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  return res.json({
+    weight,
+    height,
+    bmi: calculateBmi(height, weight),
+  });
 });
 
 const PORT = 3000;
