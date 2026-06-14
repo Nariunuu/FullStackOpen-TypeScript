3ed4895e1b2131f95e94fbf45c801acb87cc365f
khuevan2810 <khuevan2810@gmail.com>
2026-06-15 00:08:32 +0300

    6. Eslint

---
+++ healthapp/eslint.config.mjs
@@ -0,0 +1,25 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      semi: ['error', 'always'],
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'eslint.config.mjs'],
+  }
+);
