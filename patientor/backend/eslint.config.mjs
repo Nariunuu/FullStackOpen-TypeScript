e65dd2b9de0a390724ec5ca9ab87fb6cf880036c
khuevan2810 <khuevan2810@gmail.com>
2026-06-15 00:25:01 +0300

    9. Patientor backend, step1

---
+++ patientor/backend/eslint.config.mjs
@@ -0,0 +1,21 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'eslint.config.mjs'],
+  }
+);
